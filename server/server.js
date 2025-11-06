9cd8ca81d6524d54bf3eff3e17bf2266bce76f62
Cherkaoui Abdessamad <cherkaouiabdessamad9@gmail.com>
2025-11-06 11:38:22 +0100

    Update backend for production

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// server/server.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -9,25 +10,22 @@ const jobRoutes = require('./routes/jobs');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connexion à MongoDB
 connectDB();
 
-// Middleware
+// Autoriser Vercel + localhost
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+app.use(cors({ origin: [FRONTEND_URL, 'https://react-jobs-blond.vercel.app'] }));
+
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:3000' })); // frontend
 app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static('public'));
 
-// Routes
 app.use('/api/jobs', jobRoutes);
 
-// Gestion des routes non trouvées
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-// Démarrage du serveur
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
